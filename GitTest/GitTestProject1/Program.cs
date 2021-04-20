98e0c5611360e5aafd3b53bb2784aab92dd8d145
Fyodor Koryazhkin <fk250046@ncr.com>
2021-04-20 10:21:29 +0300

    Add new solution to main branch

---
+++ GitTest/GitTestProject1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTestProject1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MessageBox.Show("Message box from master.");
+		}
+	}
+}
